3eb401f63b7f8108ed9f739c2435a98c52ce3f40
Sayassat Otep <otepms@gmail.com>
2020-08-16 20:35:39 +0600

    build

---
+++ build/precache-manifest.e21ae90981a4bd2806b1f9c5cafca6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24621611696bad18cd6018fa728fd585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40d5345939d1a030ae89",
+    "url": "/static/css/main.6a3f8f9b.chunk.css"
+  },
+  {
+    "revision": "8d8d6dc8ce01e9615678",
+    "url": "/static/js/2.fb218388.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/static/js/2.fb218388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d5345939d1a030ae89",
+    "url": "/static/js/main.90ee49a1.chunk.js"
+  },
+  {
+    "revision": "0cead240beaeac72778b",
+    "url": "/static/js/runtime-main.50b5716e.js"
+  }
+]);
